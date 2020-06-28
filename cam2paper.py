4560c8c11fd832f7a14c4e03d9a7aedcd961c8ec
Kristian Kann <arrviasto@gmail.com>
2020-06-28 22:43:13 +0200

    Posterized image

--- cam2paper.py
+++ cam2paper.py
@@ -11,21 +11,42 @@ from picamera import PiCamera
 
 logging.basicConfig(level=logging.DEBUG)
 
+epd = epd4in2bc.EPD()
+size = (epd.width, epd.height)
 camera = PiCamera()
+camera.resolution = size
 camera.capture('/tmp/camera.jpg')
 
-epd = epd4in2bc.EPD()
-logging.info("init and Clear")
+logging.info("image saved")
+
+
+image = Image.open("/tmp/camera.jpg")
+image = image.convert('P', None, None, 1, 3)
+image = image.resize(size)
+
+imageY = Image.new('1', size, 0)
+imageB = Image.new('1', size, 0)
+
+for x in range(0, size[0]):
+	for y in range(0, size[1]):
+		point = (x, y)
+		if image.getpixel(point) == 1:
+			imageY.putpixel(point, 0)
+			imageB.putpixel(point, 255)
+		elif image.getpixel(point) == 2:
+			imageY.putpixel(point, 255)
+			imageB.putpixel(point, 0)
+		else:
+			imageY.putpixel(point, 255)
+			imageB.putpixel(point, 255)
+
+image.save("/tmp/paper.png")
+imageY.save("/tmp/paperY.png")
+imageB.save("/tmp/paperB.png")
+
+logging.info("image converted")
+
 epd.init()
-
-size = (epd.width, epd.height)
-
-imageY = Image.new('1', size, 255)
-imageB = Image.open("/tmp/camera.jpg")
-imageB = imageB.convert('1')
-imageB = imageB.resize(size)
-imageB.save("/tmp/paper.png")
-
 epd.display(epd.getbuffer(imageB), epd.getbuffer(imageY))
 
 epd.sleep()
